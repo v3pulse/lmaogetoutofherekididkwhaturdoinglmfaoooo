c6463e0c3b60afc11b77726ec9dfd7d9570213c3
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-07 16:18:15 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -64,7 +64,11 @@ if (!message.member.roles.find(r => r.name === "Scrim Staff")) return;
 		sentMessage.edit(nextgameEmbed);
 		await startTimeout(1000 * 60);
 	}
-	
+	let embed = new Discord.RichEmbed()
+	.setTitle("Game Information")
+	.setDescription("Loading...")
+	.setColor("#ff0000");
+	scrimlast3chan.send(embed);
 	scrimlast3chan.overwritePermissions(message.guild.id, {
 	SEND_MESSAGES: true
 	})
