a0f8542d21e0f14a59b594289769251019c2c511
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-08 19:22:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -85,8 +85,25 @@ bot.on("message", async message => {
 				
 	  		})
 			
+			message.channel.bulkDelete(1);
 			  message.member.addRole(message.guild.roles.find("name", code));
-
+			      const allCodeRoles = message.guild.roles
+        .filter(r => (/^\w{3}$/).test(r.name))
+        .sort((roleA, roleB) => roleA.name.localeCompare(roleB.name))
+        .array();
+        const SPLIT_LENGTH = 25;
+        const splitCodeRoles = [];
+        for(let i = 0; i < allCodeRoles.length; i += SPLIT_LENGTH){
+          splitCodeRoles.push(allCodeRoles.slice(i, i + SPLIT_LENGTH));
+        }
+        for(const codeRoles of splitCodeRoles) {
+          let eb = new Discord.RichEmbed().setColor(16776960).setTitle("Game Information").setFooter(`[Live] With ${allCodeRoles.length} matches.`);
+          for(const role of codeRoles) {
+            const membersString = role.members.map(m => m.user.tag).join("\n");
+            eb.addField(`ID: ${role.name}`, membersString, true);
+    }
+  
+		message.channel.send(eb);
 	  		message.guild.channels.forEach(async (channel, id) => {
 			  	await channel.overwritePermissions(scrimrole, {
 			  		SEND_MESSAGES: false,
