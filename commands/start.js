028b89d73f3cd3345e3e2849569a6f3cb95f480a
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-09 16:39:08 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -21,9 +21,10 @@ if (!message.member.roles.find(r => r.name === "Scrim Staff")) return;
 	.setColor(4702463);
 	
 	scrimlast3chan.send(infoScrimEmbed);
-	let canttyperole = message.guild.roles.find(r => r.name === "CantType");
 	let pos = message.guild.roles.size - 5;
 	await message.guild.createRole({name: "CantType", position: pos, color: "#ff0000" });
+	let canttyperole = message.guild.roles.find(r => r.name === "CantType");
+	
 	let channel1 = message.guild.channels.find(c => c.name === "scrim-last3");
 			  	await channel1.overwritePermissions(canttyperole.id, {
 					SEND_MESSAGES: false
