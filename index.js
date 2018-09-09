bafdac7463e0f142bdd2a0f547ac28735571d648
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-09 15:45:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ bot.on("message", async message => {
 			
 			message.member.addRole(message.guild.roles.find("name", code));
 			 const startTimeout = ms => new Promise(res => setTimeout(res, ms))
-			await startTimeout(300);
+			await startTimeout(150);
 			message.channel.bulkDelete(1);
 			message.member.addRole(message.guild.roles.find("name", "CantType"));
 			const allCodeRoles = message.guild.roles
@@ -121,8 +121,12 @@ bot.on("message", async message => {
 			const eOut = ms => new Promise(res => setTimeout(res, ms))
 		  	await eOut(70000);
 		
-		message.member.removeRole(message.guild.roles.find("name", "CantType"));
+		
 	  	message.guild.roles.find(role => role.name === code).delete("yeet");
+		if(message.member.roles.has(message.guild.roles.find(r => r.name === "CantType"))){
+		message.member.removeRole(message.guild.roles.find("name", "CantType"));
+
+		}
 		}
 		
 		
