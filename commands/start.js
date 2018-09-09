d938e0c116d565eb1598b4de83d8ecd21e23c958
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-09 16:21:14 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -24,7 +24,7 @@ if (!message.member.roles.find(r => r.name === "Scrim Staff")) return;
 	let pos = message.guild.roles.size - 6;
 	message.guild.createRole({name: "CantType", position: pos, color: "#ff0000" });
 	
-		message.guild.channels.find(c => c.name === "scrim-last3").replacePermissionOverwites({ overwrites: [
+		await message.guild.channels.find(c => c.name === "scrim-last3").replacePermissionOverwites({ overwrites: [
 		{
 			id: message.guild.roles.find(r => r.name === "CantType").id,
 			denied: ["SEND_MESSAGES"],
