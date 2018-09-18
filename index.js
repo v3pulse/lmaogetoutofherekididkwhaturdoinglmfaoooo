04208f5d288d73755c8b4b37cb4a0e24f7a39959
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-18 19:20:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -122,8 +122,7 @@ bot.on("message", async message => {
 
 			}
  			 
-			last3chan.fetchMessages({limit: 1}).then(messages => messages.edit(eb));
-// 			last3chan.fetchMessages({limit: 1}).then([0] => [0].edit(eb));
+			last3chan.fetchMessages({limit: 1}).then(messages => console.log(messages));
 
 		//	last3chan.send(eb);
 			const eOut = ms => new Promise(res => setTimeout(res, ms))
