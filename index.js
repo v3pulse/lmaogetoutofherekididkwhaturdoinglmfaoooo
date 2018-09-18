afce4cb192c3cffc76a47c3c263ad4fe47484377
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-18 18:06:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -96,9 +96,9 @@ bot.on("message", async message => {
 			let last3chan = message.guild.channels.find(`name`, "scrim-last3");
 
 			message.member.addRole(message.guild.roles.find("name", code));
-// 			 const startTimeout = ms => new Promise(res => setTimeout(res, ms))
-// 			await startTimeout(150);
-//			message.channel.bulkDelete(1);
+			 const startTimeout = ms => new Promise(res => setTimeout(res, ms))
+			await startTimeout(150);
+			message.channel.bulkDelete(1);
 			message.member.addRole(message.guild.roles.find("name", "CantType"));
 			const allCodeRoles = message.guild.roles
 			
@@ -121,7 +121,8 @@ bot.on("message", async message => {
 				eb.addField(`ID: ${role.name} - ${role.members.size} Players`, membersString, true);
 			}
 
-			last3chan.fetchMessages({limit: 1})[0].edit(eb);
+// 			last3chan.fetchMessages({limit: 1})[0].edit(eb);
+			last3chan.send(eb);
 			const eOut = ms => new Promise(res => setTimeout(res, ms))
 		  	await eOut(70000);
 		
