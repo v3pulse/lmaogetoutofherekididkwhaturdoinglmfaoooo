d8fd64661c546731dda28921c1e77e01f47c0516
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-19 21:58:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,13 +63,20 @@ bot.on("message", async message => {
 	  let code = message.content.toUpperCase();
 	  if(message.content === "!cls" && message.member.roles.find(r => r.name === "Scrim Staff")){
 	  	message.channel.bulkDelete(10);
+		 message.guild.roles.forEach(role => {
+			 if(/^[A-Z0-9]{3}$/.test(role.name) role.delete())
+		 });
 	  }
 
 	let last3ch = message.guild.channels.find("name", "scrim-last3");
 	  let scrimrole = message.guild.roles.find(`name`, code);
-	  if(message.member.roles.has(scrimrole)) return message.author.send("You already typed in a game code!");
+	  if(message.member.roles.has(scrimrole)) return;
 	  if(code.length != 3) return;
 	  if(/[^a-zA-Z0-9]+/g.test(code)) return;
+	  if(message.member.roles.find(r => (/^\w{3}$/).test(r.name))){
+	  message.member.removeRole(message.member.roles.find(r => (/^\w{3}$/).test(r.name)));
+	
+	  }
 	
 
 	  let nickname = message.member.nickname;
@@ -96,10 +103,10 @@ bot.on("message", async message => {
 			let last3chan = message.guild.channels.find(`name`, "scrim-last3");
 
 			message.member.addRole(message.guild.roles.find("name", code));
-			 const startTimeout = ms => new Promise(res => setTimeout(res, ms))
-			await startTimeout(150);
+// 			 const startTimeout = ms => new Promise(res => setTimeout(res, ms))
+// 			await startTimeout(150);
 			//message.channel.bulkDelete(1);
-			message.member.addRole(message.guild.roles.find("name", "CantType"));
+// 			await message.member.addRole(message.guild.roles.find("name", "CantType"));
 			const allCodeRoles = message.guild.roles
 			
 		.filter(r => (/^\w{3}$/).test(r.name))
@@ -125,11 +132,11 @@ bot.on("message", async message => {
 			last3chan.fetchMessages({limit: 1}).then(m => m.first().edit(eb));
 
 		//	last3chan.send(eb);
-			const eOut = ms => new Promise(res => setTimeout(res, ms))
-		  	await eOut(70000);
+// 			const eOut = ms => new Promise(res => setTimeout(res, ms))
+// 		  	await eOut(70000);
 		
 		
-	  	message.guild.roles.find(role => role.name === code).delete("yeet");
+// 	  	message.guild.roles.find(role => role.name === code).delete("yeet");
 
 		
 		}
